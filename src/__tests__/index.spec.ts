e473a27861585829b95f2adce25534ba23ddfc5b
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2018-05-21 21:12:46 +0100

    Finished skeleton. Added GAuth.

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,4 +1,5 @@
 describe("index", () => {
   it("is a dummy test", () => {
+    expect(true).toBeTruthy();
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import {
   CloudFrontRequest,
-  CloudFrontRequestCallback,
   CloudFrontRequestEvent,
+  CloudFrontRequestHandler,
+  CloudFrontRequestResult,
   Context,
 } from "aws-lambda";
+
+import { google } from "googleapis";
+import { parse } from "qs";
+import { promisify } from "util";
 
 import * as cookie from "cookie";
 import * as fs from "fs";
@@ -16,13 +21,13 @@ if (!process.env.JWT_SECRET) {
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_ALGO = "HS256";
 
-const isAuthenticated = (req: CloudFrontRequest) => {
+const isAuthenticated = (req: CloudFrontRequest): boolean => {
   const rawCookie = req.headers.cookie[0].value;
   const cookies = cookie.parse(rawCookie);
 
   if (cookies.jwt) {
     try {
-      jsonwebtoken.verify(cookies.jwt, JWT_SECRET, { algorithms: [ JWT_ALGO ] });
+      jsonwebtoken.verify(cookies.jwt, JWT_SECRET, { algorithms: [JWT_ALGO] });
 
       return true;
     } catch (_err) {
@@ -33,26 +38,47 @@ const isAuthenticated = (req: CloudFrontRequest) => {
   }
 };
 
-export const handler = async (
+const fsReadFile = promisify(fs.readFile);
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.REDIRECT_URL,
+);
+
+export const handler: CloudFrontRequestHandler = async (
   event: CloudFrontRequestEvent,
   _context: Context,
-  callback: CloudFrontRequestCallback,
-) => {
+): Promise<CloudFrontRequestResult> => {
   const request = event.Records[0].cf.request;
 
+  const oauthCode = request.querystring ? parse(request.querystring).code : null;
+
   if (isAuthenticated(request)) {
-    callback(null, request);
-  } else if (request) {
-    fs.readFile("public/login.html", (err, data) => {
-      if (err) {
-        console.error("failed to read login page", err);
-        throw err;
-      } else {
-        callback(null, {
-          body: data.toString("utf8"),
-          status: "401",
-        });
-      }
+    return request;
+  } else if (oauthCode) {
+    // TODO: authenticate
+    return request;
+  } else if (request.uri.endsWith("/login.php3")) {
+    const template = await fsReadFile("public/login.html");
+    const url = oauth2Client.generateAuthUrl({
+      access_type: "online",
+      scope: ["email"],
     });
+
+    return {
+      body: template.toString("utf8").replace("__LOGIN_URL__", url),
+      status: "200",
+    };
+  } else {
+    return {
+      headers: {
+        location: [{
+          key: "Location",
+          value: "/login.php3",
+        }],
+      },
+      status: "302",
+    };
   }
 };
