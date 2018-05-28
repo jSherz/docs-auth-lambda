7b380c40acc5bfbc9e9f8b9c5c89f394bf14593d
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2018-05-28 12:54:23 +0100

    Fixed vars not getting substituted in templates spec.

--- src/services/__tests__/template.spec.ts
+++ src/services/__tests__/template.spec.ts
@@ -1,6 +1,16 @@
 import { renderTemplate } from "../templates";
 
 describe("services/template", () => {
+  beforeAll(() => {
+    const varsModule = require("../../vars");
+    varsModule.GOOGLE_CLIENT_ID = "app-appy.apps.googleusercontent.com";
+    varsModule.GOOGLE_CLIENT_SECRET = "ssh-secret";
+    varsModule.JWT_SECRET = "hunter2";
+    varsModule.ROOT_URL = "https://mydocs.cloudfront.net";
+    varsModule.REDIRECT_URL = varsModule.ROOT_URL + "/login.php3";
+    varsModule.DOMAIN = "jsherz.com";
+  });
+
   describe("renderTemplate", () => {
     it("renders a template without an error", async () => {
       await expect(renderTemplate()).resolves.toMatchSnapshot();
